127037c73f01cfb0f2eff5baf0f45bdcdabb2018
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-25 00:03:14 +0530

    Update check-complete.sh

--- .github/workflows/check-complete.sh
+++ .github/workflows/check-complete.sh
@@ -5,3 +5,31 @@ if grep -q _enter *.md; then
   echo "Replace all the _enter with your input"
   exit 1
 fi
+
+checkloc() {
+  while read tok line
+  do
+    if [[ $tok =~ ^[[:digit:]] ]]
+    then
+      if [[ $tok -gt $1 ]]
+      then
+        echo $line exceeds $1 LOC;
+        exit 1;
+      fi
+    else
+      echo done;
+      exit 0;
+    fi
+  done
+}
+
+while read line
+do
+    if [[ "$line" == *"file analyzed"* ]]
+    then
+        read heading
+        read heading
+        read heading
+        checkloc $1
+    fi
+done
