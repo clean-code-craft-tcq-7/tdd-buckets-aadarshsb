7012186f93c2baddc53794f9889a6a47045751c4
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-25 00:54:00 +0530

    Create file_io.hpp

---
+++ file_io.hpp
@@ -0,0 +1,40 @@
+class FileIO
+{
+  private:
+    FILE *fp;
+  public:
+    FileIO()
+    {
+      if(this.fp == NULL)
+      {
+        printf("File pointer can't be created");
+      }
+      else
+      {
+        this.fp = f.open("log.csv","rw");
+        printf("File created Successfully");
+        fputs("Range  Readings \n",this.fp);
+      }
+    }
+  
+    void CloseFile(void) const
+    {
+      fclose(this.fp);
+    }
+    
+    void WriteDataToLine(const char& SeqStartEnd_t,const unsigned int count) const
+    {
+      std::array<std::string, 20> Range = {"\0"};
+      std::array<std::string, 8> Reading = {"\0"};
+      sprintf(Range,"%d - %d",SeqStartEnd_t.start, SeqStartEnd_t.end);
+      fputs(Range, this.fp);
+      sprintf(Reading,"%d\n",count);
+      fputs(Reading,this.fp);
+    }
+    
+    ~FileIO()
+    {
+      fclose(this.fp);
+      this.fp = NULL;
+    }
+}
