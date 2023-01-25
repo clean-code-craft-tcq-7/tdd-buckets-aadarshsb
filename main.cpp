3b4f57fb1db85fc925769fccee638acbbd629313
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-25 14:36:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@ int main(void)
   FILE *fp;
   const int sampleArraySize = 10;
   unsigned int Arr1[sampleArraySize] = {1,1,2,3,3,5,7,8,9,11};
-#if 0
+  
   if(!FileIO::ValFPAndCreateCSVFile(fp))
   {
     printf("Unable to create a file pointer \n");
     return 0;
   }
-#endif
   CCheckSeq Seq1;
   Seq1.GetChargingCurrent(Arr1, sampleArraySize);
   
