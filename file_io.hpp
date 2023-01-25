f6ce827365748ec8655f6845d86dd1190e80c461
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-25 11:11:09 +0530

    Update file_io.hpp

--- file_io.hpp
+++ file_io.hpp
@@ -3,6 +3,7 @@
 
 #include<array>
 #include<string>
+#include "check_sequence.hpp"
 
 using namespace std;
 
@@ -30,7 +31,7 @@ class FileIO
       fclose(fp);
     }
     
-    void WriteDataToLine(SeqStartEnd_t& SeqStarEnd,const unsigned int count) const
+    void WriteDataToLine(const SeqStartEnd_t& SeqStarEnd,const unsigned int count) const
     {
       char Range[20]={"\0"};
       char Reading[8]={"\0"};
