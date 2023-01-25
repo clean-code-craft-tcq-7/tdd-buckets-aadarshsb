b84ad06606723f60a33948bf35e6c489c504da4d
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-25 11:46:36 +0530

    Update check_sequence.hpp

--- check_sequence.hpp
+++ check_sequence.hpp
@@ -1,6 +1,8 @@
 #ifndef CHECK_SEQUENCE_HPP
 #define CHECK_SEQUENCE_HPP
 
+#include <array>
+
 class CCheckSeq
 {
   private:
@@ -9,14 +11,14 @@ class CCheckSeq
   typedef struct
   {
     unsigned int start;
-    uinsigned int end;
+    unsigned int end;
   }SeqStartEnd_t;
   
   private:
-    std::array<unsigned int, 40> ChargeArray;
+    std::array<unsigned int, 40> ChargeArray = {0};
     unsigned int MaxArraySize;
   public:
-    CCheckSeq():ChargeArray(0), MaxArraySize(0)
+    CCheckSeq(): MaxArraySize(0)
     {
     }
     void GetChargingCurrent(const unsigned int& CurrentReadings,const unsigned int MaximumSizeOfArray) const
@@ -27,25 +29,33 @@ class CCheckSeq
       }
       else
       {
-        this.MaxArraySize = MaximumSizeOfArray;
-        for(unsigned int Index = 0U, Index < MaxArraySize, Index++)
+        this->MaxArraySize = MaximumSizeOfArray;
+        for(unsigned int Index = 0U; Index < this->MaxArraySize; Index++)
         {
-          this.ChargeArray[Index] = CurrentReadings[Index];
+          this->ChargeArray[Index] = CurrentReadings[Index];
         }
       }
     }
     void FindSeqences()
     {
-      std::sort(ChargeArray.begin(),ChargeArray.End());
-      for(unsigned int Index = 0; Index < this.MaxArraySize-1, Index++)
+      unsigned int StartRange = 0U;
+      unsigned int EndRange = 0U;
+      unsigned int Counts = 0U;
+      std::qsort(this->ChargeArray.begin(),this->ChargeArray.End());
+      StartRange = this->ChargeArray[0];
+      for(unsigned int Index = 0; Index < this->MaxArraySize-1; Index++)
       {
+        Counts++;
         if(ChargeArray[Index + 1] >= ChargeArray[Index] + 2)
         {
+          EndRange = this->ChargeArray[Index];
+          //Call the file IO operation 
           
+          Count = 0; 
+          StartRange = this->ChargeArray[Index + 1];
         }
       }
     }
-    void CountNoOfSequences();
-}
+};
 
 #endif 
