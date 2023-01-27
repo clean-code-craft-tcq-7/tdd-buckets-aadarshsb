7e4a57a9781428ac0c90196081976aa082036c4d
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-27 12:25:25 +0530

    Update testfile.hpp

--- testfile.hpp
+++ testfile.hpp
@@ -5,24 +5,5 @@
 #include "check_sequence.hpp"
 #include <assert.h>
 
-class FileIOStub:private FileIO
-{
-  private:
-  public:
-    virtual bool ValFPAndCreateCSVFile(FILE *fp) override
-    {
-      assert(fp != NULL); 
-    }
-    virtual void CloseFile(FILE *fp) override
-    {
-      //Dummy Stub
-      assert(fp != NULL);
-    }
-    virtual void WriteDataToLine(FILE* fp,const CCheckSeq::SeqStartEnd_t& SeqStarEnd,const unsigned int count) override
-    {
-      //Dummy stub
-      assert(TRUE);
-    }
-};
 
 #endif
