3a7260d059424cf47c3936df3ded980bc7f0fa15
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-01-27 12:20:07 +0530

    Update file_io.hpp

--- file_io.hpp
+++ file_io.hpp
@@ -43,4 +43,24 @@ class FileIO
     }
 };
 
+class FileIOStub:private FileIO
+{
+  private:
+  public:
+    virtual bool ValFPAndCreateCSVFile(FILE *fp) override
+    {
+      assert(fp != NULL); 
+    }
+    virtual void CloseFile(FILE *fp) override
+    {
+      //Dummy Stub
+      assert(fp != NULL);
+    }
+    virtual void WriteDataToLine(FILE* fp,const CCheckSeq::SeqStartEnd_t& SeqStarEnd,const unsigned int count) override
+    {
+      //Dummy stub
+      assert(TRUE);
+    }
+};
+
 #endif
